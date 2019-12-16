7fa7579f78965e6a1b0d76733551da992a69bb16
Benjamin Small <ben20262@aim.com>
2019-12-16 20:48:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,35 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@song_count = 0
+  @@genres = []
+  @@artists = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@song_count += 1
+    @@genre << genre
+  end
+
+  def count
+    @@song_count
+  end
+
+  def genres
+    @@genres.uniq
+  end
+
+  def genre_count
+    genre_hash = {}
+    unique = genres()
+    unique.each do |s_genre|
+      count = 0
+      @@genres.each do |song|
+        count += 1 if s_genre == song
+      end
+      genre_hash[s_genre] = count
+    end
+    
   end
 end
